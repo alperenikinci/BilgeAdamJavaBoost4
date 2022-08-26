9d8db6be01197955847ac45e26f16b168a2767d0
Alperen İkinci <alperen.ikinci@hotmail.com>
2022-08-26 15:10:30 +0300

    Marathon Update

--- src/com/bilgeadam/boost/week02/Marathon1/ShapeTest.java
+++ src/com/bilgeadam/boost/week02/Marathon1/ShapeTest.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class ShapeTest {
 	
+	/*
+	 * Author: Alperen İKİNCİ
+	 * BilgeAdamBoostJava4 - Marathon #1 
+	 */
 	
 	public static void main(String[] args) 
 	{
 		ShapeTest shapeTest = new ShapeTest();
 		shapeTest.readInput();
 		System.out.println("Bye...");
-		
 	}
-
+	
 	private void readInput() 
 	{
 		Scanner input;
@@ -20,107 +23,109 @@ public class ShapeTest {
 		
 		int shapeNo =0;
 		boolean exit = true;
-		while(exit) 
-		{int i = 1;
-		shapeNo++;
-		sides.setSide1(0);
-		sides.setSide2(0);
-		sides.setSide3(0);
-		sides.setSide4(0);
 		
-		System.out.println("Please enter the length parameters of the shape (4 lenghth max.) to exit please type (-1) : ");
-		while(i<5)
+		while(exit)
 		{
-		double selection;
-		System.out.print("Please enter the length number "+i +" (0 to end) : " );
-		input = new Scanner(System.in);
-		selection = input.nextDouble();
+			int i = 1;
+			shapeNo++;
+			sides.setSide1(0);
+			sides.setSide2(0);
+			sides.setSide3(0);
+			sides.setSide4(0);
+			System.out.println("Please enter the length parameters of the shape (4 lenghth max.) to exit please type (-1) : ");
+			
+				while(i<5)
+				{
+					double selection;
+					System.out.print("Please enter the length number "+i +" (0 to end) : " );
+					input = new Scanner(System.in);
+					selection = input.nextDouble();
+					
+					if (selection == 0 && i<=3 && sides.getSide1()==0) 
+						{
+						System.err.println("\nGeometric shapes require at least 3 side input!!!\n");
+						continue;
+						}
+					
+					
+					if (selection == 0 && i==4 ) 
+						{
+						double diameter = calculateDiameter(sides.getSide1(),sides.getSide2(), sides.getSide3(), sides.getSide4());
+						System.out.println("\nDiameter of the shape "+shapeNo+" is : "+diameter);
+						String area = calculateArea("");
+						System.out.println("Area of the shape "+shapeNo+ " is : "+ area+" It is a triangle after all -_- \n");
+						break;
+						}
+						
+					if (sides.getSide1() == sides.getSide2() && selection == 0)
+						{
+						System.out.println("\nOh! You are looking for a square!\nHere you go pal!");
+						double diameter = calculateDiameter(sides.getSide1(),sides.getSide2(), sides.getSide3(), sides.getSide4());
+						System.out.println("\nDiameter of the shape "+shapeNo+" is : "+diameter);
+						String area = calculateArea("");
+						System.out.println("Area of the shape "+shapeNo+ " is : "+ area+" It is a square after all -_- \n");
+						break;
+						}
+					
+					if (sides.getSide1() == sides.getSide2() && selection == 0)
+						{
+						System.out.println("\nOh! You are looking for a square!\nHere you go pal!");
+						double diameter = calculateDiameter(sides.getSide1(),sides.getSide2(), sides.getSide3(), sides.getSide4());
+						System.out.println("\nDiameter of the shape "+shapeNo+" is : "+diameter);
+						String area = calculateArea("");
+						System.out.println("Area of the shape "+shapeNo+ " is : "+ area+" It is a square after all -_- \n");
+						break;
+						}	
+						
+					if (selection == -1) 
+						{
+						exit = false;
+						break;
+						}
+					else if (selection < 0) 
+						{
+						System.err.println("\nLength can not be negative!!!");
+						continue;
+						}
+					else if (selection == 0) 
+						{
+						break;
+						}
+						
+					switch (i) 
+					{
+						case 1: 
+							{
+							sides.setSide1(selection);
+							break;
+							}
+						case 2:
+							{
+							sides.setSide2(selection);
+							break;
+							}
+						case 3:
+							{
+							sides.setSide3(selection);
+							break;
+							}
+						case 4:
+							{
+							sides.setSide4(selection);
+							double diameter = calculateDiameter(sides.getSide1(),sides.getSide2(), sides.getSide3(), sides.getSide4());
+							System.out.println("\nDiameter of the shape "+shapeNo+" is : "+diameter);
+							String area = calculateArea("");
+							System.out.println("Area of the shape "+shapeNo+ " is : "+ area+" It is a rectangle after all -_- \n");
+							break;
+							}
+					}
+						i++;
+							
+				}
 		
-		if (selection == 0 && i<=3 && sides.getSide1()==0) 
-		{
-			System.err.println("\nGeometric shapes require at least 3 side input!!!\n");
-			continue;
-		}
-		
-		
-		if (selection == 0 && i==4 ) 
-		{
-			double diameter = calculateDiameter(sides.getSide1(),sides.getSide2(), sides.getSide3(), sides.getSide4());
-			System.out.println("\nDiameter of the shape "+shapeNo+" is : "+diameter);
-			String area = calculateArea("");
-			System.out.println("Area of the shape "+shapeNo+ " is : "+ area+" It is a triangle after all -_- \n");
-			break;
 		}
 			
-		if (sides.getSide1() == sides.getSide2() && selection == 0)
-		{
-			System.out.println("\nOh! You are looking for a square!\nHere you go pal!");
-			double diameter = calculateDiameter(sides.getSide1(),sides.getSide2(), sides.getSide3(), sides.getSide4());
-			System.out.println("\nDiameter of the shape "+shapeNo+" is : "+diameter);
-			String area = calculateArea("");
-			System.out.println("Area of the shape "+shapeNo+ " is : "+ area+" It is a square after all -_- \n");
-			break;
-		}
-		
-		if (sides.getSide1() == sides.getSide2() && selection == 0)
-		{
-			System.out.println("\nOh! You are looking for a square!\nHere you go pal!");
-			double diameter = calculateDiameter(sides.getSide1(),sides.getSide2(), sides.getSide3(), sides.getSide4());
-			System.out.println("\nDiameter of the shape "+shapeNo+" is : "+diameter);
-			String area = calculateArea("");
-			System.out.println("Area of the shape "+shapeNo+ " is : "+ area+" It is a square after all -_- \n");
-			break;
-		}
-		
-		
-			
-		if (selection == -1) 
-			{
-			exit = false;
-			break;
-		}
-		else if (selection < 0) 
-			{
-			System.err.println("\nLength can not be negative!!!");
-			continue;
-			}
-		else if (selection == 0) {
-			break;
-		}
-			
-			switch (i) 
-			{
-			case 1: 
-			{
-				sides.setSide1(selection);
-				break;
-			}
-			case 2:
-			{
-				sides.setSide2(selection);
-				break;
-			}
-			case 3:
-			{
-				sides.setSide3(selection);
-				break;
-			}
-			case 4:
-			{
-				sides.setSide4(selection);
-				double diameter = calculateDiameter(sides.getSide1(),sides.getSide2(), sides.getSide3(), sides.getSide4());
-				System.out.println("\nDiameter of the shape "+shapeNo+" is : "+diameter);
-				String area = calculateArea("");
-				System.out.println("Area of the shape "+shapeNo+ " is : "+ area+" It is a rectangle after all -_- \n");
-				break;
-			}
-			}
-			i++;
-		}
-		}
-		
-		
-		}
+	}
 
 	private String calculateArea(String area)
 	{
